881911fe4b6214db59b26d26ee482386648d9d24
John Obrecht <jmobrecht@gmail.com>
2022-10-30 13:05:55 -0600

    Added spiral effect and updated libraries

--- fold/sequenc_utils.py
+++ fold/sequenc_utils.py
@@ -5,6 +5,30 @@ Created on Sat Oct 29 23:07:53 2022 @author: john.obrecht
 import numpy as np
 from matplotlib import cm
 
+### Basic Functions
+
+def dist(x, y, z, x0, y0, z0, i):
+    return np.sqrt( (x - x0[i])**2 + (y - y0[i])**2 + (z - z0[i])**2)
+
+def point(x, y, z, x0, y0, z0, sz, i):
+    return np.exp(-(dist(x, y, z, x0, y0, z0, i))**2 / sz**2), 
+
+def sheet(z, z0, i):
+    return np.exp(-(z - z0[i])**2 / (0.01 * np.max(tree[:, 2]))**2)
+
+
+def rx(th):
+    th *= np.pi / 180
+    return np.array([[1, 0, 0], [0, np.cos(th), np.sin(th)], [0, -np.sin(th), np.cos(th)]])
+    
+def ry(th):
+    th *= np.pi / 180
+    return np.array([[np.cos(th), 0, np.sin(th)], [0, 1, 0], [-np.sin(th), 0, np.cos(th)]])
+
+def rz(th):
+    th *= np.pi / 180
+    return np.array([[np.cos(th), np.sin(th), 0], [-np.sin(th), np.cos(th), 0], [0, 0, 1]])
+
 # Rainbow
 def rainbow(tree, num_pts, num_frames):
     color_map = cm.get_cmap('hsv', num_pts)
@@ -16,26 +40,35 @@ def rainbow(tree, num_pts, num_frames):
 
 # Slice
 def moving_slice(tree, num_pts, num_frames):
-    h0 = np.linspace(1, 0, num_frames)
-    def slice(z, i):
-        return np.exp(-(z - h0[i])**2 / (0.01 * np.max(tree[:, 2]))**2)
+    z0 = np.linspace(1, 0, num_frames)
     seq = np.ones([num_pts, 4, num_frames])
     for i in range(1, num_frames):
-        seq[:, 3, i] = np.round(slice(tree[:, 2], i))
+        seq[:, 3, i] = np.round(sheet(tree[:, 2], z0, i))
     return seq
 
 # Rain
 def falling_rain(tree, num_pts, num_frames):
-    def dist(x, y, z, i):
-        return np.sqrt( (x - x0[i])**2 + (y - y0[i])**2 + (z - z0[i])**2)
-    def rain(x, y, z, i):
-        return np.exp(-(dist(x, y, z, i))**2 / (0.05)**2)
-    # Radial scale
-    r_sc = np.mean([-np.min(tree[:, 0]), np.max(tree[:, 0]), -np.min(tree[:, 1]), np.max(tree[:, 1])])
-    z0 = np.linspace(1, 0, num_frames)
-    y0 = np.linspace(0, 0, num_frames)
-    x0 = np.linspace(0, r_sc, num_frames)
-    seq = np.ones([num_pts, 4, num_frames])
+    x_t, y_t, z_t = tree[:, 0], tree[:, 1], tree[:, 2]
+    r_sc = np.max(np.abs(tree[:, 0:2]))  # Radial scale = largest radial extent    
+    z0 = np.linspace(1, 0, num_frames)  # Z falls from 1 to 0
+    y0 = np.linspace(0, 0, num_frames)  # Y remains fixed
+    x0 = np.linspace(0, r_sc, num_frames)  # X moves from 0 (at top) to outside at bottom
+    seq = np.ones([num_pts, 4, num_frames])  # Start all white (1, 1, 1, 1)
     for i in range(1, num_frames):
-        seq[:, 3, i] = np.round(rain(tree[:, 0], tree[:, 1], tree[:, 2], i))
+        seq[:, 3, i] = np.round(point(x_t, y_t, z_t, x0, y0, z0, 0.05, i))  # Only change col. 3 (alpha value)
+    return seq
+
+# Spiral
+def spiral(tree, num_pts, num_frames):
+    x_t, y_t, z_t = tree[:, 0], tree[:, 1], tree[:, 2]
+    r_sc = np.max(np.abs(tree[:, 0:2]))  # Radial scale = largest radial extent    
+    z0 = np.linspace(1, 0, num_frames)  # Z falls from 1 to 0
+    t0 = np.linspace(0, 2, num_frames) * 360  # Theta circles X rotations
+    r0 = np.linspace(0, r_sc, num_frames)  # R moves from 0 (at top) to outside at bottom
+    x0, y0 = np.zeros(num_frames), np.zeros(num_frames)
+    for i in range(num_frames):
+        x0[i], y0[i], _ = np.dot(rz(t0[i]), [r0[i], 0, 0])
+    seq = np.ones([num_pts, 4, num_frames])  # Start all white (1, 1, 1, 1)
+    for i in range(1, num_frames):
+        seq[:, 3, i] = np.round(point(x_t, y_t, z_t, x0, y0, z0, 0.1, i))  # Only change col. 3 (alpha value)
     return seq
--- xmas_tree.py
+++ xmas_tree.py
@@ -5,25 +5,17 @@ Created on Sun Oct 23 11:12:29 2022 @author: john.obrecht
 import re
 import os
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.animation
+from mpl_toolkits.mplot3d import Axes3D
 from fold.utils import set_axes_equal, get_tree_coords
-from fold.sequenc_utils import rainbow, moving_slice, falling_rain
-
-#%%  Load tree & properties
-
-folder = r'C:\Users\john.obrecht\Downloads\xmastree2020-main'
-file = 'coords.txt'
-path = os.sep.join([folder, file])
+from fold.sequenc_utils import rainbow, moving_slice, falling_rain, spiral
 
 # Get tree coordinates
+path = r'C:\Users\john.obrecht\Downloads\xmastree2020-main\coords.txt'
 tree = get_tree_coords(path)
 num_pts = len(tree)
-
-# Radial scale
-r_sc = np.mean([-np.min(tree[:, 0]), np.max(tree[:, 0]), -np.min(tree[:, 1]), np.max(tree[:, 1])])
-
-# XYZ scale
-tree_range = [[np.min(tree[:, 0]), np.max(tree[:, 0])], [np.min(tree[:, 1]), np.max(tree[:, 1])], [np.min(tree[:, 2]), np.max(tree[:, 2])]]
 
 #%% Animation
 
@@ -32,21 +24,18 @@ def update_color(i):
     title.set_text('Frame: {} of {}'.format(i, num_frames))
 
 # Sequence properties
-num_frames = 500
+num_frames = 100
 
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.animation
-import pandas as pd
+# Load sequence
+seq = spiral(tree, num_pts, num_frames)
 
-seq = rainbow(tree, num_pts, num_frames)
-
+# Animation frame rate: how fast the animation progresses through sequence
 frame_rate = 0.01
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.set_facecolor((0.2, 0.2, 0.2))
 title = ax.set_title('3D Test')
-
 graph = ax.scatter(tree[:, 0], tree[:, 1], tree[:, 2], s=4, c = seq[:, :, 0])
 graph._offsets3d = (tree[:, 0], tree[:, 1], tree[:, 2])
 ax.set_xlim(np.min(tree[:, 0]), np.max(tree[:, 0]))
@@ -58,22 +47,3 @@ ani = matplotlib.animation.FuncAnimation(fig, update_color, num_frames, interval
 plt.show()
 
 # ani.save('rainbow.gif')
-
-#%% Static View
-
-# import matplotlib.pyplot as plt
-
-# fig = plt.figure(figsize=(6, 6))
-# ax = fig.add_subplot(projection='3d')
-
-# ax.set_facecolor((0.2, 0.2, 0.2))
-# ax.scatter(tree[:, 0], tree[:, 1], tree[:, 2], s=4, c=xp, cmap='jet')
-# ax.set_xlim(np.min(tree[:, 0]), np.max(tree[:, 0]))
-# ax.set_ylim(np.min(tree[:, 1]), np.max(tree[:, 1]))
-# ax.set_zlim(np.min(tree[:, 2]), np.max(tree[:, 2]))
-# set_axes_equal(ax)
-# ax.axis('off')
-# fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
-# plt.show()
-
-#%%
